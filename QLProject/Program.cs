6fb0c27689391319ec383da2c3a639014ee0b288
Sainoo <122950791+Sainoo19@users.noreply.github.com>
2023-06-09 17:09:35 +0700

    Update

--- QLProject/Program.cs
+++ QLProject/Program.cs
@@ -16,7 +16,7 @@ namespace QLProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fManager());
+            Application.Run(new fLogin());
         }
     }
 }
